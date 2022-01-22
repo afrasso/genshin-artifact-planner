10143d6b0fac6dbbb7d6fca6434fd71b2c423c25
Anthony Frasso <afrasso@cimpress.com>
2022-01-22 14:56:39 -0500

    Removing Semantic UI.

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import "semantic-ui-css/semantic.css";
+import Character from "./Character.tsx";
+
+function App() {
+  return <Character />;
+}
+
+export default App;
---
+++ src/Character.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Character extends React.Component {
+  render() {
+    return <div>Character select list goes here!</div>;
+  }
+}
+
+export default Character;
